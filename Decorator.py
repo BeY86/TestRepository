f6299c13ba3ba687831f8fa9d99a1ff1aa24fa61
BeY86 <davidmoiseev07@gmail.com>
2023-09-02 01:04:43 +0300

    New class BankAccount

--- Decorator.py
+++ Decorator.py
@@ -1,15 +1,40 @@
-def Decorator(func):
-    def wrapper():
-        print("Start Programm!")
-        func()
-        print("Stop Programm!")
+class BankAccount():
+    def __init__(self, balance):
+        self.balance = balance
 
-    return wrapper
+    def deposit(self, amount):
+        if amount > 0:
+            self.balance += amount
+            return True
+        else:
+            return False
+        
+    def withdraw(self,amount):
+        if amount > 0:
+            self.balance -= amount
+            return True
+        else:
+            return False
+    def get_balance(self):
+        print(f"Ваш баланс: {self.balance}")
 
+account = BankAccount(0)
 
-@Decorator
-def show():
-    print("Hello World!")
+command = ["deposit", "withdraw", "balance", "quit",]
 
+while True:
+    cmd = input("Введите команду:")
+    if cmd == "deposit":
+        amount = float(input("Введите сумму пополнения баланса: "))
+        account.deposit(amount)
+    if cmd == "withdraw":
+        amount = float(input("Введите сумму снятия средств с баланса: "))
+        account.withdraw(amount)
+    if cmd == "balance":
+        account.get_balance()
+    if cmd == "quit":
+        break
+    if not cmd in command:
+        print("Error Command!")
 
-show()
+            
